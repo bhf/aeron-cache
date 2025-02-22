4751f1f8d8556b1c7a84295d6eeaafe9af055135
bhf <sanjeev.sarda@gmail.com>
2025-02-22 15:53:45 +0000

    AC-54 Update for multiple http interfaces

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,6 @@ rootProject.name = "aeron-cache"
 include("cache-cluster")
 include("cache-client")
 include("cache-common")
-include("cache-http-server")
 include("cache-messages-sbe")
 include("cache-messages-http")
+include("cache-http:http-server-javalin")
