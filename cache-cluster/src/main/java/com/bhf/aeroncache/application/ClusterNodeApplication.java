076af24c962a2728acfac6cd5ea8b55672ea7c51
bhf <sanjeev.sarda@gmail.com>
2025-03-01 15:20:15 +0000

    Handle DNS resolution delay

--- cache-cluster/src/main/java/com/bhf/aeroncache/application/ClusterNodeApplication.java
+++ cache-cluster/src/main/java/com/bhf/aeroncache/application/ClusterNodeApplication.java
@@ -16,8 +16,11 @@ import lombok.extern.log4j.Log4j2;
 import org.agrona.ErrorHandler;
 import org.agrona.concurrent.NoOpLock;
 import org.agrona.concurrent.ShutdownSignalBarrier;
+import org.agrona.concurrent.SystemEpochClock;
 
 import java.io.File;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -181,6 +184,13 @@ public class ClusterNodeApplication {
                         .clusteredService(new SBEDecodingCacheClusterService())
                         .errorHandler(errorHandler("Clustered Service"));
 
+        System.out.println("Awaiting DNS Resolution");
+        final List<String> hostAddresses = List.of(hostnames);
+
+        for (int i = 0; i < hostAddresses.size(); i++) {
+            awaitDnsResolution(hostAddresses, i);
+        }
+
         System.out.println("Launching cluster node now...");
 
         try (
@@ -192,5 +202,59 @@ public class ClusterNodeApplication {
             barrier.await();
             System.out.println("[" + nodeId + "] Exiting");
         }
+    }
+
+    private static void awaitDnsResolution(final List<String> hostArray, final int nodeId) {
+        if (applyDnsDelay()) {
+            System.out.println("Waiting 5 seconds for DNS to be registered...");
+            quietSleep(5000);
+        }
+
+        final long endTime = SystemEpochClock.INSTANCE.time() + 60000;
+        final String nodeName = hostArray.get(nodeId);
+        java.security.Security.setProperty("networkaddress.cache.ttl", "0");
+
+        boolean resolved = false;
+        while (!resolved) {
+            if (SystemEpochClock.INSTANCE.time() > endTime) {
+                System.out.println("cannot resolve name "+nodeName+", exiting");
+                System.exit(-1);
+            }
+
+            try {
+                var res = InetAddress.getByName(nodeName);
+                resolved = true;
+                System.out.println("Resolved "+nodeName+" to "+res);
+            } catch (final UnknownHostException e) {
+                System.out.println("cannot yet resolve name "+nodeName+", retrying in 3 seconds");
+                quietSleep(3000);
+            }
+        }
+    }
+
+    /**
+     * Sleeps for the given number of milliseconds, ignoring any interrupts.
+     *
+     * @param millis the number of milliseconds to sleep.
+     */
+    private static void quietSleep(final long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (final InterruptedException ex) {
+            System.out.println("Interrupted while sleeping");
+        }
+    }
+
+    /**
+     * Apply DNS delay
+     *
+     * @return true if DNS delay should be applied
+     */
+    private static boolean applyDnsDelay() {
+        final String dnsDelay = System.getenv("DNS_DELAY");
+        if (null == dnsDelay || dnsDelay.isEmpty()) {
+            return false;
+        }
+        return Boolean.parseBoolean(dnsDelay);
     }
 }
