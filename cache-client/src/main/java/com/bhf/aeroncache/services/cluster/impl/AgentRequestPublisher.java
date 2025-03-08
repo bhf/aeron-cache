594e8905ea48c9f67488a99970190d5160090348
bhf <sanjeev.sarda@gmail.com>
2025-03-08 17:15:13 +0000

    Push encoded messages onto the ringbuffer

--- cache-client/src/main/java/com/bhf/aeroncache/services/cluster/impl/AgentRequestPublisher.java
+++ cache-client/src/main/java/com/bhf/aeroncache/services/cluster/impl/AgentRequestPublisher.java
@@ -1,7 +1,6 @@
 package com.bhf.aeroncache.services.cluster.impl;
 
-import com.bhf.aeroncache.messages.AddCacheEntryEncoder;
-import com.bhf.aeroncache.messages.MessageHeaderEncoder;
+import com.bhf.aeroncache.messages.*;
 import io.aeron.cluster.client.AeronCluster;
 import lombok.RequiredArgsConstructor;
 import org.agrona.concurrent.ringbuffer.ManyToOneRingBuffer;
@@ -16,7 +15,32 @@ public class AgentRequestPublisher extends ClusterMessagePublisher {
     final ManyToOneRingBuffer rb;
 
     @Override
-    public void publishAddCachEntry(AeronCluster cluster, AddCacheEntryEncoder addCacheEntry, MessageHeaderEncoder header) {
+    void publishAddCachEntry(AeronCluster cluster, AddCacheEntryEncoder addCacheEntry, MessageHeaderEncoder header) {
         rb.write(0, addCacheEntry.buffer(), 0, addCacheEntry.encodedLength()+header.encodedLength());
+    }
+
+    @Override
+    public void publishCreateCache(AeronCluster cluster, CreateCacheEncoder createCache, MessageHeaderEncoder header) {
+        rb.write(0, createCache.buffer(), 0, createCache.encodedLength()+header.encodedLength());
+    }
+
+    @Override
+    void publishGetCacheEntry(AeronCluster cluster, GetCacheEntryEncoder getCacheEntry, MessageHeaderEncoder header) {
+        rb.write(0, getCacheEntry.buffer(), 0, getCacheEntry.encodedLength()+header.encodedLength());
+    }
+
+    @Override
+    void publishClearCache(AeronCluster cluster, ClearCacheEncoder clearCache, MessageHeaderEncoder header) {
+        rb.write(0, clearCache.buffer(), 0, clearCache.encodedLength()+header.encodedLength());
+    }
+
+    @Override
+    void publishDeleteCache(AeronCluster cluster, DeleteCacheEncoder deleteCache, MessageHeaderEncoder header) {
+        rb.write(0, deleteCache.buffer(), 0, deleteCache.encodedLength()+header.encodedLength());
+    }
+
+    @Override
+    void publishRemoveCacheEntry(AeronCluster cluster, RemoveCacheEntryEncoder removeCacheEntry, MessageHeaderEncoder header) {
+        rb.write(0, removeCacheEntry.buffer(), 0, removeCacheEntry.encodedLength()+header.encodedLength());
     }
 }
