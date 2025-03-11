6c1c91065aeec7c445bf7a4c28cc45928e2b57a7
bhf <sanjeev.sarda@gmail.com>
2025-03-11 07:31:59 +0000

    Added requestId onto publishers

--- cache-client/src/main/java/com/bhf/aeroncache/services/cluster/ClusterRequestPublisher.java
+++ cache-client/src/main/java/com/bhf/aeroncache/services/cluster/ClusterRequestPublisher.java
@@ -9,107 +9,119 @@ public interface ClusterRequestPublisher {
     /**
      * Send a message to create a cache instance.
      *
-     * @param cluster The Aeron Cluster instance to use.
-     * @param cacheId The ID of the cache to create.
+     * @param cluster   The Aeron Cluster instance to use.
+     * @param requestId The Id of this request.
+     * @param cacheId   The ID of the cache to create.
      */
-    void sendCreateCache(AeronCluster cluster, long cacheId);
+    void sendCreateCache(AeronCluster cluster, String requestId, long cacheId);
 
     /**
      * Send a message to create a cache instance, block
      * until you get a response.
      *
-     * @param cluster The Aeron Cluster instance to use.
-     * @param cacheId The ID of the cache to create.
+     * @param cluster   The Aeron Cluster instance to use.
+     * @param requestId The Id of this request.
+     * @param cacheId   The ID of the cache to create.
      */
-    void sendCreateCacheBlocking(AeronCluster cluster, long cacheId);
+    void sendCreateCacheBlocking(AeronCluster cluster, String requestId, long cacheId);
 
     /**
      * Send a message to add a cache entry in a non-blocking manner.
      *
-     * @param cluster The Aeron Cluster instance to use.
-     * @param cacheId The ID of the cache we're adding too.
-     * @param key     The key to use.
-     * @param value   The value to use.
+     * @param cluster   The Aeron Cluster instance to use.
+     * @param requestId The Id of this request.
+     * @param cacheId   The ID of the cache we're adding too.
+     * @param key       The key to use.
+     * @param value     The value to use.
      */
-    void addCacheEntry(AeronCluster cluster, long cacheId, String key, String value);
+    void addCacheEntry(AeronCluster cluster, String requestId, long cacheId, String key, String value);
 
     /**
      * Send a message to add a cache entry and block
      * until you get the result back.
      *
-     * @param cluster The Aeron Cluster instance to use.
-     * @param cacheId The ID of the cache we're adding too.
-     * @param key     The key to use.
-     * @param value   The value to use.
+     * @param cluster   The Aeron Cluster instance to use.
+     * @param requestId The Id of this request.
+     * @param cacheId   The ID of the cache we're adding too.
+     * @param key       The key to use.
+     * @param value     The value to use.
      */
-    void addCacheEntryBlocking(AeronCluster cluster, long cacheId, String key, String value);
+    void addCacheEntryBlocking(AeronCluster cluster, String requestId, long cacheId, String key, String value);
 
     /**
      * Send a message to get a cache entry.
      *
-     * @param cluster The Aeron Cluster instance to use.
-     * @param cacheId The ID of the cache we're adding too.
-     * @param key     The key to use.
+     * @param cluster   The Aeron Cluster instance to use.
+     * @param requestId The Id of this request.
+     * @param cacheId   The ID of the cache we're adding too.
+     * @param key       The key to use.
      */
-    void getCacheEntry(AeronCluster cluster, long cacheId, String key);
+    void getCacheEntry(AeronCluster cluster, String requestId, long cacheId, String key);
 
     /**
      * Send a message to get a cache entry synchronously.
      *
-     * @param cluster The Aeron Cluster instance to use.
-     * @param cacheId The ID of the cache we're adding too.
-     * @param key     The key to use.
+     * @param cluster   The Aeron Cluster instance to use.
+     * @param requestId The Id of this request.
+     * @param cacheId   The ID of the cache we're adding too.
+     * @param key       The key to use.
      */
-    void getCacheEntryBlocking(AeronCluster cluster, long cacheId, String key);
+    void getCacheEntryBlocking(AeronCluster cluster, String requestId, long cacheId, String key);
 
     /**
      * Send a message to clear a cache.
      *
-     * @param cluster The Aeron Cluster instance to use.
-     * @param cacheId The ID of the cache we're clearing out.
+     * @param cluster   The Aeron Cluster instance to use.
+     * @param requestId The Id of this request.
+     * @param cacheId   The ID of the cache we're clearing out.
      */
-    void clearCache(AeronCluster cluster, long cacheId);
+    void clearCache(AeronCluster cluster, String requestId, long cacheId);
 
     /**
      * Send a message to clear a cache. Blocks until it gets a response.
      *
-     * @param cluster The Aeron Cluster instance to use.
-     * @param cacheId The ID of the cache we're clearing out.
+     * @param cluster   The Aeron Cluster instance to use.
+     * @param requestId The Id of this request.
+     * @param cacheId   The ID of the cache we're clearing out.
      */
-    void clearCacheBlocking(AeronCluster cluster, long cacheId);
+    void clearCacheBlocking(AeronCluster cluster, String requestId, long cacheId);
 
     /**
      * Send a message to delete a cache.
      *
-     * @param cluster The Aeron Cluster instance to use.
-     * @param cacheId The ID of the cache we're deleting.
+     * @param cluster   The Aeron Cluster instance to use.
+     * @param requestId The Id of this request.
+     * @param cacheId   The ID of the cache we're deleting.
      */
-    void deleteCache(AeronCluster cluster, long cacheId);
+    void deleteCache(AeronCluster cluster, String requestId, long cacheId);
 
     /**
      * Send a message to delete a cache in a blocking manner.
      *
-     * @param cluster The Aeron Cluster instance to use.
-     * @param cacheId The ID of the cache we're deleting.
+     * @param cluster   The Aeron Cluster instance to use.
+     * @param requestId The Id of this request.
+     * @param cacheId   The ID of the cache we're deleting.
      */
-    void deleteCacheBlocking(AeronCluster cluster, long cacheId);
+    void deleteCacheBlocking(AeronCluster cluster, String requestId, long cacheId);
 
     /**
      * Send a message to remove a cache entry.
      *
-     * @param cluster The Aeron Cluster instance to use.
-     * @param cacheId The ID of the cache we're removing an entry from.
-     * @param key     The key of the entry we're removing.
+     * @param cluster   The Aeron Cluster instance to use.
+     * @param requestId The Id of this request.
+     * @param cacheId   The ID of the cache we're removing an entry from.
+     * @param key       The key of the entry we're removing.
      */
-    void removeCacheEntry(AeronCluster cluster, long cacheId, String key);
+    void removeCacheEntry(AeronCluster cluster, String requestId, long cacheId, String key);
 
     /**
      * Send a message to remove a cache entry in a blocking manner.
      *
-     * @param cluster The Aeron Cluster instance to use.
-     * @param cacheId The ID of the cache we're removing an entry from.
-     * @param key     The key of the entry we're removing.
+     * @param cluster   The Aeron Cluster instance to use.
+     * @param requestId The Id of this request.
+     * @param cacheId   The ID of the cache we're removing an entry from.
+     * @param key       The key of the entry we're removing.
      */
-    void removeCacheEntryBlocking(AeronCluster cluster, long cacheId, String key);
+    void removeCacheEntryBlocking(AeronCluster cluster, String requestId, long cacheId, String key);
 
 }
