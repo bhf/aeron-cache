19a34c098dff5f3d038d4bc7dfdb028064d59b59
bhf <sanjeev.sarda@gmail.com>
2025-03-29 16:08:15 +0000

    Add page structure

---
+++ cache-ui/nextjs/src/app/cache/[slug]/page.tsx
@@ -0,0 +1,13 @@
+export default async function Page() {
+
+    return (
+        <>
+            <h1>Cache Functionality</h1>
+            <li>View this cache</li>
+            <li>Delete this cache</li>
+            <li>Clear this cache</li>
+            <li>Add Item</li>
+            <li>Remove by key</li>
+        </>
+    )
+}
---
+++ cache-ui/nextjs/src/app/caches/page.tsx
@@ -0,0 +1,11 @@
+export default async function Page() {
+
+    return (
+        <>
+            <h1>All Caches</h1>
+            <li>View specific cache</li>
+            <li>Table of all caches</li>
+            <li>Create cache</li>
+        </>
+    )
+}
