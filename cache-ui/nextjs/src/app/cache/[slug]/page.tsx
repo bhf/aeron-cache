3443d5c4e49f7f17e6f98f8a02f304f5f301f0a9
bhf <sanjeev.sarda@gmail.com>
2025-03-30 12:41:35 +0100

    Add page structure

--- cache-ui/nextjs/src/app/cache/[slug]/page.tsx
+++ cache-ui/nextjs/src/app/cache/[slug]/page.tsx
@@ -1,13 +1,88 @@
+/**
+ * A component to delete a cache.
+ * @constructor
+ */
+function DeleteCache() {
+    return (
+        <div>
+        </div>
+    );
+}
+
+/**
+ * A component to clear a cache.
+ * @constructor
+ */
+function ClearCache() {
+    return (
+        <div>
+        </div>
+    );
+}
+
+/**
+ * A component to add an item.
+ * @constructor
+ */
+function AddItem() {
+    return (
+        <div>
+        </div>
+    );
+}
+
+/**
+ * A component to remove a cache item by it's key.
+ * @constructor
+ */
+function RemoveByKey() {
+    return (
+        <div>
+        </div>
+    );
+}
+
+/**
+ * A component to display the items in this cache.
+ * @constructor
+ */
+function CacheItemsTable() {
+    return (
+        <div>
+        </div>
+    );
+}
+
+/**
+ * A page to display information on a specific Aeron Cache instance.
+ * @constructor
+ */
 export default async function Page() {
 
+    /**
+     * Functional stateless component, can be simplified
+     * further into a lambda with implicit return.
+     *
+     * @param value The value to display in the heading.
+     * @constructor
+     */
+    const Headline = ({value}) => {
+        return <h1>{value}</h1>;
+    };
+
     return (
-        <>
-            <h1>Cache Functionality</h1>
-            <li>View this cache</li>
-            <li>Delete this cache</li>
-            <li>Clear this cache</li>
-            <li>Add Item</li>
-            <li>Remove by key</li>
-        </>
+        <div>
+            <Headline value={"Cache Functionality"}/>
+            <h2>Delete this cache</h2>
+            <DeleteCache/>
+            <h2>Clear this cache</h2>
+            <ClearCache/>
+            <h2>Add Item</h2>
+            <AddItem/>
+            <h2>Remove by key</h2>
+            <RemoveByKey/>
+            <h2>View items in this cache</h2>
+            <CacheItemsTable/>
+        </div>
     )
 }
--- cache-ui/nextjs/src/app/caches/page.tsx
+++ cache-ui/nextjs/src/app/caches/page.tsx
@@ -1,11 +1,38 @@
-export default async function Page() {
-
+/**
+ * A component to display a table of available caches.
+ * @constructor
+ */
+function CacheTable() {
     return (
-        <>
+        <div>
+        </div>
+    );
+}
+
+/**
+ * A component to create caches.
+ * @constructor
+ */
+function CreateCache() {
+    return (
+        <div>
+        </div>
+    );
+}
+
+/**
+ * A page to display a summary of existing Aeron Cache instances and
+ * to allow the user to create a cache.
+ * @constructor
+ */
+export default async function Page() {
+    return (
+        <div>
             <h1>All Caches</h1>
-            <li>View specific cache</li>
-            <li>Table of all caches</li>
-            <li>Create cache</li>
-        </>
-    )
+            <h2>Create cache</h2>
+            <CreateCache/>
+            <h2>All Caches</h2>
+            <CacheTable/>
+        </div>
+    );
 }
