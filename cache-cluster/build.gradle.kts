63fd016868e8a8694f888734f852e314187d22c7
bhf <sanjeev.sarda@gmail.com>
2025-04-17 19:53:41 +0100

    Cluster side otel tracing

--- cache-cluster/build.gradle.kts
+++ cache-cluster/build.gradle.kts
@@ -12,6 +12,10 @@ dependencies {
     implementation(libs.log4j.core)
     implementation(project(":cache-messages-sbe"))
     implementation(project(":cache-common"))
+    implementation("io.opentelemetry:opentelemetry-api:1.49.0")
+    implementation("io.opentelemetry:opentelemetry-sdk:1.49.0")
+    implementation("io.opentelemetry:opentelemetry-exporter-otlp:1.49.0")
+
     testImplementation(platform(libs.junit.bom))
     testImplementation(libs.junit)
     testImplementation(libs.junit.params)
