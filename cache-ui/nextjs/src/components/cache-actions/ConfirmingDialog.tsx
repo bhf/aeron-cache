70224fa3106fe3b0a798024e84e406d45af4ebc6
bhf <sanjeev.sarda@gmail.com>
2025-06-21 13:33:01 +0100

    Revert to fix build issue

--- cache-ui/nextjs/src/components/cache-actions/ConfirmingDialog.tsx
+++ cache-ui/nextjs/src/components/cache-actions/ConfirmingDialog.tsx
@@ -18,7 +18,7 @@ interface ConfirmingDialogProps {
     cancelText: string
     actionText: string
     buttonText: string
-    action: () => Promise<never>
+    action: () => Promise<void>
 }
 
 /**
