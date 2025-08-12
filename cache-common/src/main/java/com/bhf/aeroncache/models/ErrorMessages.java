f235f11a8fd305c2cf20c464724a602d4555bc89
bhf <sanjeev.sarda@gmail.com>
2025-08-12 11:19:40 +0100

    HTTP side validation of cache ID on creates

--- cache-common/src/main/java/com/bhf/aeroncache/models/ErrorMessages.java
+++ cache-common/src/main/java/com/bhf/aeroncache/models/ErrorMessages.java
@@ -5,11 +5,11 @@ package com.bhf.aeroncache.models;
  */
 public class ErrorMessages {
 
-    public static final String USE_NUMERIC_CACH_ID = "Cache Ids should be numeric.";
-
     public static final String KEYS_SHOULD_BE_STRING = "Keys should be a string.";
 
     public static final String VALUES_SHOULD_BE_STRING = "Values should be a string.";
 
     public static final String CHECK_ALL_VALUES = "Check your request params are correct.";
+
+    public static final String CACHE_ID_NO_SPECIAL_CHARACTERS = "Cache Id shouldn't include special characters.";
 }
