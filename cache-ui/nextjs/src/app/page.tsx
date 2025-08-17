5d0f4341751e9e0a061420d1c9e4b36054c357a6
bhf <sanjeev.sarda@gmail.com>
2025-08-17 19:11:09 +0100

    Default pages for hard refresh and direct navigation

--- cache-ui/nextjs/src/app/page.tsx
+++ cache-ui/nextjs/src/app/page.tsx
@@ -1,82 +1,8 @@
-import {allCachesColumns} from "@/components/cache-summary/AllCachesColumns";
-import {AllCachesDataTable} from "@/components/cache-summary/AllCachesTable";
-import CreateCacheRequest from "@/components/CreateCache";
-import {Card, CardContent, CardDescription, CardHeader, CardTitle,} from "@/components/ui/card"
-import {getCacheAPIURI} from "@/lib/actions";
-import {getLogger} from "@/lib/loggingUtil";
-
-const logger = getLogger("MainDash")
-
-const headers = {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-}
-
-/**
- * A component to display a table of available caches.
- * @constructor
- */
-async function CacheTable() {
-
-    let rawResponse
-    try {
-        rawResponse = await fetch(await getCacheAPIURI() + '/caches/', {
-                method: 'GET',
-                headers,
-                next: { tags: ['AllCaches'] },
-                cache: "no-cache"
-            },
-        )
-    } catch (err) {
-        logger.warn("Error whilst sending request to get cache items ", err);
-        return
-    }
-
-    const content = await rawResponse.json();
-    logger.info("Got response from get all caches ", content)
-
-    return (
-        <AllCachesDataTable columns={allCachesColumns} data={content}/>
-    );
-}
-
-/**
- * A component to create caches.
- * @constructor
- */
-function CreateCache() {
-    return (
-        <CreateCacheRequest/>
-    );
-}
-
 export default async function Page() {
 
     return (
         <div>
-            <div className="pt-4">
-                <div className="pb-6 px-6 md:w-1/2">
-                    <Card className={"shadow-lg"}>
-                        <CardHeader>
-                            <CardTitle>Create Cache</CardTitle>
-                            <CardDescription>Create a new cache</CardDescription>
-                        </CardHeader>
-                        <CardContent>
-                            <CreateCache/>
-                        </CardContent>
-                    </Card>
-                </div>
-                <div className="pb-6 px-6 md:w-1/2">
-                    <Card className={"shadow-lg"}>
-                        <CardHeader>
-                            <CardTitle>All Caches</CardTitle>
-                        </CardHeader>
-                        <CardContent>
-                            <CacheTable/>
-                        </CardContent>
-                    </Card>
-                </div>
-            </div>
+
         </div>
     )
 }
