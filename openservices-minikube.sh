bc575d5cbf3dec51bb49df54ebd53a9d9cfeb81f
bhf <sanjeev.sarda@gmail.com>
2026-01-24 16:20:41 +0000

    Correct minikube services script

--- openservices-minikube.sh
+++ openservices-minikube.sh
@@ -1,3 +1,5 @@
-minikube service aeroncache-http --url --profile aeroncache
-minikube service aeroncache-ws-javalin --url --profile aeroncache
-minikube service aeroncache-ui-nextjs --url --profile aeroncache
+minikube -n aeroncache service aeroncache-http-javalin --url --profile aeroncache
+minikube -n aeroncache service aeroncache-http-near-javalin --url --profile aeroncache
+minikube -n aeroncache service aeroncache-ws-javalin --url --profile aeroncache
+minikube -n aeroncache service aeroncache-sse-jooby --url --profile aeroncache
+minikube -n aeroncache service aeroncache-ui-nextjs --url --profile aeroncache
