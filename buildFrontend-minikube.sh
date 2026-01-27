d2e99786fb0e43ea33a75136c39713cfbad0411d
bhf <sanjeev.sarda@gmail.com>
2026-01-27 08:51:18 +0000

    Remove superfluous bash scripts

--- buildFrontend-minikube.sh
+++
@@ -1,3 +0,0 @@
-cd cache-ui/nextjs
-docker build . -t aeroncache-ui-nextjs
-minikube image load aeroncache-ui-nextjs:latest --profile aeroncache
--- openservices-minikube.sh
+++
@@ -1,5 +0,0 @@
-minikube -n aeroncache service aeroncache-http-javalin --url --profile aeroncache
-minikube -n aeroncache service aeroncache-http-near-javalin --url --profile aeroncache
-minikube -n aeroncache service aeroncache-ws-javalin --url --profile aeroncache
-minikube -n aeroncache service aeroncache-sse-jooby --url --profile aeroncache
-minikube -n aeroncache service aeroncache-ui-nextjs --url --profile aeroncache
