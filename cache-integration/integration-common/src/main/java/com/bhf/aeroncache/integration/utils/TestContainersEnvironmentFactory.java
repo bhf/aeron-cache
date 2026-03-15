9801d5bb1da9d60ce9991074f681af035e17933c
bhf <sanjeev.sarda@gmail.com>
2026-03-15 17:16:42 +0000

    Add non-clustered TestContainers environments

--- cache-integration/integration-common/src/main/java/com/bhf/aeroncache/integration/utils/TestContainersEnvironmentFactory.java
+++ cache-integration/integration-common/src/main/java/com/bhf/aeroncache/integration/utils/TestContainersEnvironmentFactory.java
@@ -138,6 +138,100 @@ public class TestContainersEnvironmentFactory {
     }
 
     /**
+     * Get a container for a single AeronCache node.
+     *
+     * @param network
+     * @return
+     */
+    public static GenericContainer<?> getSingleNodeCacheContainer(Network network) {
+        String name = "node0";
+        String hostPath = "/tmp/aeron-cache/" + name + "-" + UUID.randomUUID();
+        new File(hostPath).mkdirs();
+
+        return new GenericContainer<>("aeroncache-cluster:latest")
+                .withNetwork(network)
+                .withNetworkAliases(name)
+                .withCreateContainerCmdModifier(cmd -> cmd.withHostName(name))
+                .withFileSystemBind(hostPath, "/tmp/data", BindMode.READ_WRITE)
+                .withSharedMemorySize(SHM_SIZE_MBS * 1024L * 1024L)
+                .withEnv("JAVA_TOOL_OPTIONS", "-Daeron.debug.timeout=60s")
+                .withEnv("CLUSTER_NODE", "0")
+                .withEnv("OTEL_SERVICE_NAME", "aeron-cache-cluster-node")
+                .withEnv("OTEL_EXPORTER_OTLP_ENDPOINT", "http://jaeger:4317")
+                .withEnv("CACHE_MODE", "SINGLE")
+                .withEnv("CACHE_DATA_DIR", "/tmp/data")
+                .withEnv("HTTP_RESPONSE_PUB_HOST", "cache-http-client")
+                .withEnv("WS_RESPONSE_PUB_HOST", "cache-ws-client")
+                .withEnv("SSE_RESPONSE_PUB_HOST", "cache-sse-client");
+    }
+
+    /**
+     * Get a container for a single HTTP interface configured for single node mode.
+     *
+     * @param network
+     * @return
+     */
+    public static GenericContainer<?> getSingleNodeHTTPContainer(Network network) {
+        return new GenericContainer<>("aeroncache-http-javalin:latest")
+                .withNetwork(network)
+                .withNetworkAliases("cache-http-client")
+                .withSharedMemorySize(SHM_SIZE_MBS * 1024L * 1024L)
+                .withExposedPorts(7070)
+                .withEnv("JAVA_TOOL_OPTIONS", "-Daeron.debug.timeout=60s")
+                .withEnv("OTEL_SERVICE_NAME", "aeron-cache-http")
+                .withEnv("OTEL_EXPORTER_OTLP_ENDPOINT", "http://jaeger:4318")
+                .withEnv("OTEL_JAVAAGENT_LOGGING", "none")
+                .withEnv("CLUSTER_ADDRESSES", "node0")
+                .withEnv("CACHE_MODE", "SINGLE")
+                .withEnv("REQUEST_PUB_HOST", "node0")
+                .waitingFor(Wait.forHttp("/readiness"));
+    }
+
+    /**
+     * Get a container for a single WS interface configured for single node mode.
+     *
+     * @param network
+     * @return
+     */
+    public static GenericContainer<?> getSingleNodeWSContainer(Network network) {
+        return new GenericContainer<>("aeroncache-ws-javalin:latest")
+                .withNetwork(network)
+                .withNetworkAliases("cache-ws-client")
+                .withSharedMemorySize(SHM_SIZE_MBS * 1024L * 1024L)
+                .withExposedPorts(7071)
+                .withEnv("JAVA_TOOL_OPTIONS", "-Daeron.debug.timeout=60s")
+                .withEnv("OTEL_SERVICE_NAME", "aeron-cache-ws")
+                .withEnv("OTEL_EXPORTER_OTLP_ENDPOINT", "http://jaeger:4318")
+                .withEnv("OTEL_JAVAAGENT_LOGGING", "none")
+                .withEnv("CLUSTER_ADDRESSES", "node0")
+                .withEnv("CACHE_MODE", "SINGLE")
+                .withEnv("REQUEST_PUB_HOST", "node0")
+                .waitingFor(Wait.forHttp("/readiness"));
+    }
+
+    /**
+     * Get a container for a single SSE interface configured for single node mode.
+     *
+     * @param network
+     * @return
+     */
+    public static GenericContainer<?> getSingleNodeSSEContainer(Network network) {
+        return new GenericContainer<>("aeroncache-sse-jooby:latest")
+                .withNetwork(network)
+                .withNetworkAliases("cache-sse-client")
+                .withSharedMemorySize(SHM_SIZE_MBS * 1024L * 1024L)
+                .withExposedPorts(7072)
+                .withEnv("JAVA_TOOL_OPTIONS", "-Daeron.debug.timeout=60s")
+                .withEnv("OTEL_SERVICE_NAME", "aeron-cache-sse")
+                .withEnv("OTEL_EXPORTER_OTLP_ENDPOINT", "http://jaeger:4318")
+                .withEnv("OTEL_JAVAAGENT_LOGGING", "none")
+                .withEnv("CLUSTER_ADDRESSES", "node0")
+                .withEnv("CACHE_MODE", "SINGLE")
+                .withEnv("REQUEST_PUB_HOST", "node0")
+                .waitingFor(Wait.forHttp("/readiness"));
+    }
+
+    /**
      * Build the AeronCache cluster addresses based on the number of nodes.
      *
      * @param nodes
