86ded7423dd3ddd63d45f908e962e92fb6dd3a8c
bhf <sanjeev.sarda@gmail.com>
2026-03-21 17:14:48 +0000

    Extract map cache into SPI implementation module

---
+++ cache-spi-impl/map-cache/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+}
+
+group = "com.bhf.aeroncache"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.aeron)
+    implementation(libs.log4j.api)
+    implementation(libs.log4j.core)
+
+    implementation(project(":cache-spi"))
+    implementation(project(":cache-common"))
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
