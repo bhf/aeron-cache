525fe5177abd9c12e3230ddcfea92263b32bc5fd
bhf <sanjeev.sarda@gmail.com>
2026-04-18 16:30:39 +0100

    Cache admin actions component

---
+++ cache-ui/nextjs/src/components/CacheAdminActions.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import {Button} from "@/components/ui/button";
+import {CameraIcon, ZapOffIcon} from "lucide-react";
+import {toast} from "sonner";
+
+interface CacheAdminActionsProps {
+    baseUri?: string
+}
+
+export default function CacheAdminActions({baseUri}: CacheAdminActionsProps) {
+    const handleSnapshot = async () => {
+        try {
+            const res = await fetch(baseUri+'/api/v1/snapshot', {method: 'POST'});
+            if (!res.ok) throw new Error("Couldn't take a snapshot");
+            const props = {
+                title: "Snapshot Requested",
+                description: "Snapshot successfully requested",
+                actionLabel: "OK"
+            };
+            toast(props.title, {
+                description: props.description,
+                action: {
+                    label: props.actionLabel,
+                    onClick: () => {
+                    },
+                },
+            })
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        } catch (err) {
+            const props = {
+                title: "Snapshot Request Error",
+                description: "Error requesting snapshot",
+                actionLabel: "OK"
+            };
+            toast(props.title, {
+                description: props.description,
+                action: {
+                    label: props.actionLabel,
+                    onClick: () => {
+                    },
+                },
+            })
+        }
+    };
+
+    const handleShutdown = async () => {
+        try {
+            const res = await fetch(baseUri+'/api/v1/shutdown', {method: 'POST'});
+            if (!res.ok) throw new Error("Couldn't request shutdown");
+            const props = {
+                title: "Shutdown Requested",
+                description: "Shutdown successfully requested",
+                actionLabel: "OK"
+            };
+            toast(props.title, {
+                description: props.description,
+                action: {
+                    label: props.actionLabel,
+                    onClick: () => {
+                    },
+                },
+            })
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        } catch (err) {
+            const props = {
+                title: "Shutdown Request Error",
+                description: "Error requesting shutdown",
+                actionLabel: "OK"
+            };
+            toast(props.title, {
+                description: props.description,
+                action: {
+                    label: props.actionLabel,
+                    onClick: () => {
+                    },
+                },
+            })
+        }
+    };
+
+    return (
+        <>
+            <Button variant="link" onClick={handleSnapshot}>
+                <CameraIcon/>Snapshot
+            </Button>
+            <Button variant="link" onClick={handleShutdown}>
+                <ZapOffIcon/>Shutdown
+            </Button>
+        </>
+    );
+}
--- cache-ui/nextjs/src/components/MenuLinks.tsx
+++ cache-ui/nextjs/src/components/MenuLinks.tsx
@@ -3,12 +3,14 @@ import Link from "next/link";
 import {Button} from "@/components/ui/button";
 import {LayersIcon, TrelloIcon} from "lucide-react";
 import {getJaegerURL, getPrometheusURL} from "@/lib/actions";
+import CacheAdminActions from "@/components/CacheAdminActions";
 
 export default async function MenuLinks() {
     const jaegerURL = await getJaegerURL()
     const prometheuesURL = await getPrometheusURL()
+    const adminBaseURI = process.env.AERON_CACHE_API
 
-    console.log("Jaeger url:"+jaegerURL+", Prometheus URL:"+prometheuesURL);
+    console.log("Jaeger url:"+jaegerURL+", Prometheus URL:"+prometheuesURL+", Admin Base URI:"+adminBaseURI);
 
     return (
         <div className="hidden lg:flex lg:flex-1 lg:justify-end pr-4">
@@ -17,6 +19,7 @@ export default async function MenuLinks() {
                 variant="link"><LayersIcon/>Jaeger</Button></Link>
             <Link href={prometheuesURL} target="_blank"><Button
                 variant="link"><TrelloIcon/>Prometheus</Button></Link>
+            <CacheAdminActions baseUri = {adminBaseURI}/>
         </div>
     )
 }
