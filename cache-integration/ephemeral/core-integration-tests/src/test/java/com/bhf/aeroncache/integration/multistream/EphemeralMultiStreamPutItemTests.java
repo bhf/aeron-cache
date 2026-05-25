414b797a39badb07d92ea6be4a9d2901d7191034
bhf <sanjeev.sarda@gmail.com>
2026-05-25 16:03:37 +0100

    Integration test for ordered removals based on ttl

--- cache-integration/ephemeral/core-integration-tests/src/test/java/com/bhf/aeroncache/integration/multistream/EphemeralMultiStreamPutItemTests.java
+++ cache-integration/ephemeral/core-integration-tests/src/test/java/com/bhf/aeroncache/integration/multistream/EphemeralMultiStreamPutItemTests.java
@@ -1,5 +1,6 @@
 package com.bhf.aeroncache.integration.multistream;
 
+import com.bhf.aeroncache.integration.BackendTestResource;
 import com.bhf.aeroncache.integration.config.BackendTestConfig;
 import com.bhf.aeroncache.integration.streaming.AbstractMultiStreamPutItemTests;
 import com.bhf.aeroncache.integration.streaming.SSEStreamingHelper;
@@ -10,5 +11,10 @@ class EphemeralMultiStreamPutItemTests extends AbstractMultiStreamPutItemTests {
 
     public EphemeralMultiStreamPutItemTests() {
         super(new SSEStreamingHelper(), new WSStreamingHelper());
+    }
+
+    @Override
+    protected void shouldGetOrderedUpdatesOnTimedRemoved(BackendTestResource backend) {
+        // not supported for ephemeral caches
     }
 }
--- cache-integration/integration-common-streaming/src/main/java/com/bhf/aeroncache/integration/streaming/AbstractMultiStreamPutItemTests.java
+++ cache-integration/integration-common-streaming/src/main/java/com/bhf/aeroncache/integration/streaming/AbstractMultiStreamPutItemTests.java
@@ -21,6 +21,8 @@ public abstract class AbstractMultiStreamPutItemTests {
     private static final String KNOWN_CACHE_ID = "1";
     private static final String KNOWN_KEY = "SomeKey";
     private static final String KNOWN_VALUE = "SomeValue";
+
+    private static final String ANOTHER_KNOWN_KEY = "SomeOtherKey";
     private static final String ANOTHER_KNOWN_VALUE = "SomeOtherValue";
 
     private final StreamingHelper[] streamingHelpers;
@@ -91,6 +93,49 @@ public abstract class AbstractMultiStreamPutItemTests {
             MatcherAssert.assertThat(secondUpdateEvent.cacheId(), Matchers.is(KNOWN_CACHE_ID));
             MatcherAssert.assertThat(secondUpdateEvent.itemKey(), Matchers.is(KNOWN_KEY));
             MatcherAssert.assertThat(secondUpdateEvent.itemValue(), Matchers.is(ANOTHER_KNOWN_VALUE));
+        }
+    }
+
+    @Test
+    @DisplayName("Should get ordered add and remove streaming updates on put items with a ttl")
+    @HappyPath
+    protected void shouldGetOrderedUpdatesOnTimedRemoved(BackendTestResource backend) {
+        // Arrange
+        var perStreamingSourceEvents = StreamingHelperUtil.getPerStreamEvents(streamingHelpers, backend, KNOWN_CACHE_ID, 4);
+
+        // Act
+        CacheTestUtils.addItem(KNOWN_CACHE_ID, KNOWN_KEY, KNOWN_VALUE, 5000, backend);
+        CacheTestUtils.addItem(KNOWN_CACHE_ID, ANOTHER_KNOWN_KEY, ANOTHER_KNOWN_VALUE, 6000, backend);
+
+        // Assert
+        for (var streamingSourceEventsFuture : perStreamingSourceEvents) {
+            Awaitility.await()
+                    .atMost(60, TimeUnit.SECONDS)
+                    .until(streamingSourceEventsFuture::isDone);
+
+            var streamingSoureEvents = streamingSourceEventsFuture.join();
+
+            var updateEvent = streamingSoureEvents.get(0);
+            MatcherAssert.assertThat(updateEvent.eventType(), Matchers.is(CacheUpdateEvent.EventType.ADD_ITEM));
+            MatcherAssert.assertThat(updateEvent.cacheId(), Matchers.is(KNOWN_CACHE_ID));
+            MatcherAssert.assertThat(updateEvent.itemKey(), Matchers.is(KNOWN_KEY));
+            MatcherAssert.assertThat(updateEvent.itemValue(), Matchers.is(KNOWN_VALUE));
+
+            var secondUpdateEvent = streamingSoureEvents.get(1);
+            MatcherAssert.assertThat(secondUpdateEvent.eventType(), Matchers.is(CacheUpdateEvent.EventType.ADD_ITEM));
+            MatcherAssert.assertThat(secondUpdateEvent.cacheId(), Matchers.is(KNOWN_CACHE_ID));
+            MatcherAssert.assertThat(secondUpdateEvent.itemKey(), Matchers.is(ANOTHER_KNOWN_KEY));
+            MatcherAssert.assertThat(secondUpdateEvent.itemValue(), Matchers.is(ANOTHER_KNOWN_VALUE));
+
+            var firstRemove = streamingSoureEvents.get(2);
+            MatcherAssert.assertThat(firstRemove.eventType(), Matchers.is(CacheUpdateEvent.EventType.REMOVE_ITEM));
+            MatcherAssert.assertThat(firstRemove.cacheId(), Matchers.is(KNOWN_CACHE_ID));
+            MatcherAssert.assertThat(firstRemove.itemKey(), Matchers.is(KNOWN_KEY));
+
+            var secondRemove = streamingSoureEvents.get(3);
+            MatcherAssert.assertThat(secondRemove.eventType(), Matchers.is(CacheUpdateEvent.EventType.REMOVE_ITEM));
+            MatcherAssert.assertThat(secondRemove.cacheId(), Matchers.is(KNOWN_CACHE_ID));
+            MatcherAssert.assertThat(secondRemove.itemKey(), Matchers.is(ANOTHER_KNOWN_KEY));
         }
     }
 
