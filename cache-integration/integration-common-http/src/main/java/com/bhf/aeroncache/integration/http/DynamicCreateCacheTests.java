fab06916900d11981844c901c36c71b313eae991
bhf <sanjeev.sarda@gmail.com>
2026-06-03 19:11:54 +0100

    IT for dynamic cache creation

--- cache-integration/integration-common-http/src/main/java/com/bhf/aeroncache/integration/http/DynamicCreateCacheTests.java
+++ cache-integration/integration-common-http/src/main/java/com/bhf/aeroncache/integration/http/DynamicCreateCacheTests.java
@@ -7,6 +7,7 @@ import com.bhf.aeroncache.integration.utils.CacheTestUtils;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.hamcrest.Matchers;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -53,8 +54,49 @@ abstract class DynamicCreateCacheTests {
     @DisplayName("Should create cache dynamically in a bulk op")
     @HappyPath
     void shouldCreateCacheDynamicallyInBulkOp(BackendTestResource backend) {
+        // Arrange
+        var dynamicBulkCacheId = "bulk-dynamic-1";
+        CacheTestUtils.deleteCache(dynamicBulkCacheId, backend);
 
+        var op1 = CacheTestUtils.getCacheOperation("req-1", "ADD_ITEM", dynamicBulkCacheId, KNOWN_KEY, KNOWN_VALUE, 0);
+        var op2 = CacheTestUtils.getCacheOperation("req-2", "ADD_ITEM", dynamicBulkCacheId, "AnotherKey", "AnotherValue", 0);
+        var ops = new JSONArray().put(op1).put(op2);
+        
+        var requestBody = new JSONObject()
+                .put("requestId", "bulk-req-dynamic")
+                .put("operations", ops);
 
+        // Act
+        RestAssured.given().port(backend.getHttpPort()).baseUri(backend.getBaseHttpUri())
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(requestBody.toString())
+                .when().post("/api/v1/cache/bulkops/")
+
+                // Assert
+                .then().assertThat()
+                .statusCode(200)
+                .body("requestId", Matchers.comparesEqualTo("bulk-req-dynamic"))
+                .body("operationResponses", Matchers.hasSize(2))
+                .body("operationResponses[0].status", Matchers.comparesEqualTo("SUCCESS"))
+                .body("operationResponses[1].status", Matchers.comparesEqualTo("SUCCESS"));
+
+        // Verify items were actually added
+        RestAssured.given().port(backend.getHttpPort()).baseUri(backend.getBaseHttpUri())
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when().get(BASE_API + dynamicBulkCacheId + "/" + KNOWN_KEY)
+                .then().assertThat()
+                .statusCode(200)
+                .body("value", Matchers.comparesEqualTo(KNOWN_VALUE));
+
+        RestAssured.given().port(backend.getHttpPort()).baseUri(backend.getBaseHttpUri())
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when().get(BASE_API + dynamicBulkCacheId + "/AnotherKey")
+                .then().assertThat()
+                .statusCode(200)
+                .body("value", Matchers.comparesEqualTo("AnotherValue"));
     }
 
 }
