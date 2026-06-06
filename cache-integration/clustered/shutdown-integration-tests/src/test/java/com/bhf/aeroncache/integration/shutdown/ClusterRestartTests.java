a3336b0928c18c4dba1a0012eb707c2a0625cbe8
Sanjeev Sarda <sanjeev.sarda@gmail.com>
2026-06-06 11:41:40 +0100

    Single timer per cache-key combo

--- cache-integration/clustered/shutdown-integration-tests/src/test/java/com/bhf/aeroncache/integration/shutdown/ClusterRestartTests.java
+++ cache-integration/clustered/shutdown-integration-tests/src/test/java/com/bhf/aeroncache/integration/shutdown/ClusterRestartTests.java
@@ -13,6 +13,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+/**
+ * Note that tests for restart are combined in a single class in order to
+ * have a faster integration test suite - the restart tests are typically the slowest
+ * to complete so become the bottleneck.
+ */
 @ExtendWith(BackendTestLauncher.class)
 @BackendTestConfig(httpEnabled = true, wsEnabled = false, sseEnabled = false, useClusteredMode = true, useTestContainersEnvironment = true)
 class ClusterRestartTests {
@@ -23,7 +28,11 @@ class ClusterRestartTests {
     static final String KNOWN_VALUE = "SomeValue★★★";
     static final String TTL_KEY = "TtlKey★★★";
     static final String TTL_VALUE = "TtlValue★★★";
+    static final String LONG_TTL_KEY = "NewTtlKey★★★";
+    static final String LONG_TTL_VALUE = "NewTtlValue★★★";
     static final long TTL_MS = 2000L;
+    static final long LONG_TTL_MS = 3600000L;
+    static final long SHORT_TTL_MS = 1000L;
 
     @Test
     @DisplayName("Should get known non-expired value we added post restart")
@@ -33,6 +42,7 @@ class ClusterRestartTests {
         CacheTestUtils.createCache(KNOWN_CACHE_ID, backend);
         CacheTestUtils.addItem(KNOWN_CACHE_ID, KNOWN_KEY, KNOWN_VALUE, backend);
         CacheTestUtils.addItem(KNOWN_CACHE_ID, TTL_KEY, TTL_VALUE, TTL_MS, backend);
+        CacheTestUtils.addItem(KNOWN_CACHE_ID, LONG_TTL_KEY, LONG_TTL_VALUE, LONG_TTL_MS, backend);
 
         // Act
         ContainerRestartUtils.stopHTTPInterface(backend);
@@ -69,6 +79,44 @@ class ClusterRestartTests {
                 .then().assertThat()
                 .statusCode(404);
 
+        RestAssured.given().port(mappedPort)
+                .baseUri(mappedHost)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when().get(GET_ENDPOINT + KNOWN_CACHE_ID + "/" + LONG_TTL_KEY)
+                .then().assertThat()
+                .statusCode(200)
+                .body("value", Matchers.comparesEqualTo(LONG_TTL_VALUE));
+
+        canModifyTtlPostRestart(backend, mappedPort, mappedHost);
+    }
+
+    /**
+     * Check we can modify and expire an item post restart.
+     *
+     * @param backend
+     * @param mappedPort
+     * @param mappedHost
+     */
+    private static void canModifyTtlPostRestart(BackendTestResource backend, Integer mappedPort, String mappedHost) {
+        // Arrange - modify the LONG_TTL to now become the SHORT_TTL so the item expires
+        CacheTestUtils.addItem(KNOWN_CACHE_ID, LONG_TTL_KEY, LONG_TTL_VALUE, SHORT_TTL_MS, backend);
+
+        // Act - sleep for the same time as the TTL of the item just to be sure
+        try {
+            Thread.sleep(SHORT_TTL_MS + 100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Assert
+        RestAssured.given().port(mappedPort)
+                .baseUri(mappedHost)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when().get(GET_ENDPOINT + KNOWN_CACHE_ID + "/" + LONG_TTL_KEY)
+                .then().assertThat()
+                .statusCode(404);
     }
 
 }
