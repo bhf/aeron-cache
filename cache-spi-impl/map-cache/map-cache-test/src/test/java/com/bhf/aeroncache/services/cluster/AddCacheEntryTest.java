e6721f964f1cb26b5849d35b1b888e6e2bf89c4d
bhf <sanjeev.sarda@gmail.com>
2026-06-12 19:51:46 +0100

    Improve UT

--- cache-spi-impl/map-cache/map-cache-test/src/test/java/com/bhf/aeroncache/services/cluster/AddCacheEntryTest.java
+++ cache-spi-impl/map-cache/map-cache-test/src/test/java/com/bhf/aeroncache/services/cluster/AddCacheEntryTest.java
@@ -13,9 +13,11 @@ import com.bhf.aeroncache.services.tracing.CacheTracingService;
 import com.bhf.aeroncache.types.ReusableString;
 import com.bhf.aeroncache.utils.SupplierUtils;
 import io.aeron.cluster.service.ClientSession;
+import io.aeron.cluster.service.Cluster;
 import io.aeron.logbuffer.Header;
 import org.agrona.ExpandableArrayBuffer;
 import org.agrona.MutableDirectBuffer;
+import org.agrona.concurrent.BackoffIdleStrategy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -28,8 +30,7 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 /**
  * Test decoding an add cache entry request.
@@ -44,11 +45,14 @@ class AddCacheEntryTest {
     private CacheTracingService tracingService;
     private final CacheRequestEncoder cacheRequestEncoder = new RegularStringCacheRequestEncoder();
     private final CacheResponseDecoder cacheResponseDecoder = new ReusableStringCacheResponseDecoder();
+    private final Cluster cluster = Mockito.mock(Cluster.class);
 
     @BeforeEach
     void setup() {
         tracingService = Mockito.mock(CacheTracingService.class);
         sut = new SBEDecodingCacheClusterService("node0", tracingService, TestUtils.getCacheManagerFactory());
+        sut.onStart(cluster, null);
+
         sut.subscriptionService = Mockito.mock(CacheSubscriptionService.class);
         responseBuffer = new ExpandableArrayBuffer();
         requestBuffer = new ExpandableArrayBuffer();
@@ -65,7 +69,7 @@ class AddCacheEntryTest {
         ClientSession session = TestUtils.getMockedSession(responseBuffer);
         var key = "someKey★★★";
         var value = "someValue★★★";
-        var ttl = 0;
+        var ttl = 10000;
 
         // create the cache
         TestUtils.createCache(cacheId, session, requestBuffer, sut);
@@ -87,6 +91,9 @@ class AddCacheEntryTest {
         verify(tracingService, times(1)).startAddCacheEntry(any(AddCacheEntryRequestDetails.class));
         verify(tracingService, times(1)).endAddCacheEntry(any(AddCacheEntryRequestDetails.class));
 
+        // we schedule a timer for the ttl
+        verify(cluster, times(1)).scheduleTimer(anyLong(), anyLong());
+
         // Call the subscription service on the back of adding an entry
         var reusableKey = new ReusableString();
         reusableKey.copyFrom(key);
@@ -100,6 +107,47 @@ class AddCacheEntryTest {
                 eq(reusableKey),
                 eq(reusableValue),
                 anyInt());
+    }
+
+    @ParameterizedTest
+    @DisplayName("Should reschedule ttl")
+    @ValueSource(strings = {"testCacheId-ttl-resched"})
+    @HappyPath
+    void shouldRescheduleTTL(String cacheId) {
+        // Arrange
+        ClientSession session = TestUtils.getMockedSession(responseBuffer);
+        var key = "someKey★★★";
+        var value = "someValue★★★";
+        var ttl = 10000;
+
+        // create the cache
+        TestUtils.createCache(cacheId, session, requestBuffer, sut);
+
+        // add an entry to the cache
+        var requestId = UUID.randomUUID().toString();
+        var length = cacheRequestEncoder.encodeAddCacheEntry(requestId, cacheId, key, value, ttl, requestBuffer);
+        sut.onSessionMessage(session, System.currentTimeMillis(), requestBuffer, 0, length, header);
+
+        // Act
+        requestId = UUID.randomUUID().toString();
+        ttl*=2;
+        length = cacheRequestEncoder.encodeAddCacheEntry(requestId, cacheId, key, value, ttl, requestBuffer);
+        sut.onSessionMessage(session, System.currentTimeMillis(), requestBuffer, 0, length, header);
+
+        cacheResponseDecoder.decodeAddCacheEntryResult(responseBuffer, 0, result);
+
+        // Assert
+        assertEquals(cacheId, result.getCacheId().value());
+        assertEquals(requestId, result.getRequestId());
+        assertEquals(CacheOperationStatus.SUCCESS, result.getStatus());
+
+
+        // we scheduled a timer for the ttl and rescheduled it
+        verify(cluster, times(2)).scheduleTimer(anyLong(), anyLong());
+
+        // we cancelled the timer before we rescheduled it
+        verify(cluster, times(1)).cancelTimer(anyLong());
+
     }
 
     @Test
@@ -123,6 +171,34 @@ class AddCacheEntryTest {
         assertEquals(cacheId, result.getCacheId().value());
         assertEquals(requestId, result.getRequestId());
         assertEquals(CacheOperationStatus.UNKNOWN_CACHE, result.getStatus());
+    }
+
+    @ParameterizedTest
+    @DisplayName("Should add entry to a dynamically created cache")
+    @ValueSource(strings = {"Some-Uncreated-Cache"})
+    @HappyPath
+    void shouldAddToDynamicallyCreatedCache(String cacheId) {
+        // Arrange
+        ClientSession session = TestUtils.getMockedSession(responseBuffer);
+        var key = "someKey★★★";
+        var value = "someValue★★★";
+        var ttl = 10000;
+
+        sut.setDynamicCacheCreationEnabled(true);
+
+        // add an entry to the cache
+        var requestId = UUID.randomUUID().toString();
+        var length = cacheRequestEncoder.encodeAddCacheEntry(requestId, cacheId, key, value, ttl, requestBuffer);
+
+        // Act
+        sut.onSessionMessage(session, System.currentTimeMillis(), requestBuffer, 0, length, header);
+        cacheResponseDecoder.decodeAddCacheEntryResult(responseBuffer, 0, result);
+
+        // Assert
+        assertEquals(cacheId, result.getCacheId().value());
+        assertEquals(requestId, result.getRequestId());
+        assertEquals(CacheOperationStatus.SUCCESS, result.getStatus());
+
     }
 
 }
